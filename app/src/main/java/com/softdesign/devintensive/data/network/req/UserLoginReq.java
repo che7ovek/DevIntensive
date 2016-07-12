5af2ccfe55aec11d9dc53c5a2b0298070ab8dd8b
che7ovek <che7ovek@yandex.ru>
2016-07-12 22:47:06 +0300

    Задания без звёздочек

---
+++ app/src/main/java/com/softdesign/devintensive/data/network/req/UserLoginReq.java
@@ -0,0 +1,12 @@
+package com.softdesign.devintensive.data.network.req;
+
+public class UserLoginReq {
+
+    private String email;
+    private String password;
+
+    public UserLoginReq(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/softdesign/devintensive/utils/AppConfig.java
@@ -0,0 +1,5 @@
+package com.softdesign.devintensive.utils;
+
+public interface AppConfig {
+    String BASE_URL = "http://devintensive.softdesign-apps.ru/api/";
+}
